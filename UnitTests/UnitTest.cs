8e1c1707c09eb722e4e6269a025ff02308cb349c
Dennis Lindblad <delindblad@gmail.com>
2026-09-18 11:44:52 +0200

    Add UnitTests project with initial test for TodoList menu structure

---
+++ UnitTests/UnitTest.cs
@@ -0,0 +1,22 @@
+﻿namespace TestProject1;
+
+public class UnitTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var root = new TodoList.RootMenu("Main Menu");
+        var child1 = new TodoList.SimpleChildPage("Child Page 1", root);
+        var child2 = new TodoList.SimpleChildPage("Child Page 2", root);
+
+        root.AddChildPage(child1);
+        root.AddChildPage(child2);
+
+        Assert.Equal("Main Menu", root.Title);
+        Assert.Equal(2, root.ChildPages.Count);
+        Assert.Contains(child1, root.ChildPages);
+        Assert.Contains(child2, root.ChildPages);
+        Assert.Equal(root, child1.Parent);
+        Assert.Equal(root, child2.Parent);
+    }
+}
